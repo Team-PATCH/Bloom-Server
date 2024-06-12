b06d80cffa11d2bdafa3c21a2b1256ff82bbcd1b
WaterBean <gkstnqls9@gmail.com>
2024-06-12 15:24:56 +0900

    [Fix] 블롭스토리지 직접 입력

--- azureBlobClient.js
+++ azureBlobClient.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const blobServiceClient = BlobServiceClient.fromConnectionString('DefaultEndpointsProtocol=https;AccountName=strbloom;AccountKey=MwTCKg0zDrblyizMhaqKLBmQnIesp5V95uUz6DP8SuTx3Avks3t5MawYwPZdyKzCkisAR4SM8BBc+AStiJCmDw==;EndpointSuffix=core.windows.net');
-const containerName = process.env.CONTAINER_NAME;
+const containerName = 'product';
 
 const getBlobUrl = async (blobName) => {
     const containerClient = blobServiceClient.getContainerClient(containerName);
