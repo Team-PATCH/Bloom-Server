ed79abbeb4379642c68cf7835424c5fff27e4811
WaterBean <gkstnqls9@gmail.com>
2024-06-12 09:11:49 +0900

    [Feat] 업로드 된 이미지 API로 호출 테스트 완료

--- azureBlobClient.js
+++ azureBlobClient.js
@@ -1,1 +1,15 @@
-azureBlobClient.js
+const { BlobServiceClient } = require('@azure/storage-blob');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+const containerName = process.env.AZURE_CONTAINER_NAME;
+
+const getBlobUrl = async (blobName) => {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobClient = containerClient.getBlobClient(blobName);
+    return blobClient.url;
+};
+
+module.exports = { getBlobUrl };
